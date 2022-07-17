53e209d86181944645152b8f0d74b881cc962f40
Renan Fonseca da Silva <rfonsecadasilva@gmail.com>
2022-07-17 22:49:42 +0800

    Add table output
    Add table output
    Fix error ins 3D output

--- src/swash_pp/swash_mat2nc.py
+++ src/swash_pp/swash_mat2nc.py
@@ -22,9 +22,7 @@ def swashdict(path_sc=""):
         return swash_unit
 
     if path_sc!="":
-        #path_sc="/mnt/d/Renan/Renan-PhD/SWASH/Git_proj/swash-7.01_further/code/"
         init = open(path_sc+"SwashInit.ftn90","r").readlines()
-        print("test here")
     else:
         import urllib.request
         import tarfile
@@ -85,7 +83,21 @@ def load_req(path_run):
     for f in fname:
         frame[f]=[[np.linspace(float(i.split()[3]),float(i.split()[3])+float(i.split()[6]),int(i.split()[8])+1) for i  in sws if i[:3]=='FRA' and  f==i.split()[1]][0], #y
                             [np.linspace(float(i.split()[2]),float(i.split()[2])+float(i.split()[5]),int(i.split()[7])+1) for i  in sws if i[:3]=='FRA' and  f==i.split()[1]][0]] #x
-    
+    # make list with table
+    fname=[i.split()[1] for i in sws if i[:3]=='POI'] #point names
+    point={} # id, y and x
+    for f in fname:
+        for i in sws:
+            if i[:3]=='POI' and f==i.split()[1]:
+                if i.split()[2][0].isdigit():
+                    point[f]=[[float(j) for j in i.split()[3:][::2]],[float(j) for j in i.split()[2:][::2]]]
+                else:
+                    with open(path_run+i.split()[2],"r").readlines() as tab:
+                        point[f]=[[j.strip().split()[1] for j in tab],[j.strip().split()[0] for j in tab]]
+    # cgrid for regular grid
+    cgrid={"'COMPGRID'":[np.linspace(float(i.split()[3]),float(i.split()[3])+float(i.split()[6]),int(i.split()[8])+1), #y
+                            np.linspace(float(i.split()[2]),float(i.split()[2])+float(i.split()[5]),int(i.split()[7])+1)]
+                for i in sws if i[:5]=='CGRID' and i.split()[1][:3]=='REG'}
     # cgrid for regular grid
     cgrid={"'COMPGRID'":[np.linspace(float(i.split()[3]),float(i.split()[3])+float(i.split()[6]),int(i.split()[8])+1), #y
                             np.linspace(float(i.split()[2]),float(i.split()[2])+float(i.split()[5]),int(i.split()[7])+1)]
@@ -100,19 +112,19 @@ def load_req(path_run):
                 out= dict(filter(lambda item:item[0] in ('Xp','Yp'),out.items())) # basic filter
                 cgrid={"'COMPGRID'":[list(out['Yp'][:,0][::-1]),list(out['Xp'][0,:])]}
                 frame={**frame,**cgrid}
-    return frame,reqn,nV
+    return frame,reqn,nV,reqtable,point
 
 
 def mat2nc_mean_2D(path_run,path_sc=""):
     """
-    Save nc with dataset mean 2D
+    Save nc with dataset mean 2D (one nc for all output)
     Iterates over output requests and create single nc file with all mean quantities.
     Note: Only tested for multiple requests with one grid.
     path_sc is the path of SWASH source code if using developer mode.
     """
     import scipy.io as sio
     import xarray as xr
-    frame,reqn,_=load_req(path_run)
+    frame,reqn,_,_,_=load_req(path_run)
     swash_dict,attr_dict=swashdict(path_sc=path_sc)
     out={}
     [out.update(sio.loadmat(path_run+mat[2])) for index,mat in enumerate(reqn) if reqn[index][1]=="mean" or 'XP' in reqn[index][3] or 'YP' in reqn[index][3]] # read only 'man' data
@@ -137,7 +149,7 @@ def mat2nc_mean_2D(path_run,path_sc=""):
 
 def mat2nc_mean_3D(path_run,path_sc=""):
     """
-    Save nc with dataset mean 3D
+    Save nc with dataset mean 3D (one nc for all output)
     Iterates over output requests and create single nc file with all mean quantities.
     Note: Only tested for multiple requests with one grid (e.g. COMPGRID).
     path_sc is the path of SWASH source code if using developer mode.
@@ -145,12 +157,12 @@ def mat2nc_mean_3D(path_run,path_sc=""):
     import scipy.io as sio
     import xarray as xr
     import numpy as np
-    frame,reqn,nV=load_req(path_run)
+    frame,reqn,nV,_,_=load_req(path_run)
     swash_dict,attr_dict=swashdict(path_sc=path_sc)
     out={}
     [out.update(sio.loadmat(path_run+mat[2])) for index,mat in enumerate(reqn) if reqn[index][1]=="mean" or 'XP' in reqn[index][3] or 'YP' in reqn[index][3]] # read only 'mean' data
     out= dict(filter(lambda item:item[0] not in ('__header__', '__version__', '__globals__'),out.items())) # basic filter
-    okv = list(set([i[:-1-nV//10] for i in dict(filter(lambda item:item[0][-1].isdigit(),out.items())).keys()])) # filter 3D data (ends with digit), will be datarray name
+    okv = list(set([i[:-1-int(np.log10(nV))] for i in dict(filter(lambda item:item[0][-1].isdigit(),out.items())).keys()])) # filter 3D data (ends with digit), will be datarray name
     okv_k=[[int(i[len(j):]) for i in out.keys() if i[:len(j)]==j] for j in okv] # will be k axis
     out_k= [[value for i,value in out.items() if i[:len(j)]==j] for j in okv] #will be datarray
     if out_k:
@@ -178,14 +190,14 @@ def mat2nc_mean_3D(path_run,path_sc=""):
 
 def mat2nc_ins_2D(path_run,path_sc=""):
     """
-    Save nc with dataset instantaneous 2D
+    Save nc with dataset instantaneous 2D (one nc for each variable)
     Iterates over output requests and create one nc file for every variable.
     path_sc is the path of SWASH source code if using developer mode.
     """
     import scipy.io as sio
     import xarray as xr
     import numpy as np
-    frame,reqn,_=load_req(path_run)
+    frame,reqn,_,_,_=load_req(path_run)
     swash_dict,attr_dict=swashdict(path_sc=path_sc)
     ivl=[] # var, y,  x and output (.mat)
     [[ivl.append([j,frame.get(i[0],['']*2)[0],frame.get(i[0],['']*2)[1],i[2]]) for j in  i[3] if j not in ['XP','YP','BOTL'] ] for i in reqn if i[1]=="inst"] # instantaneous variables list
@@ -217,14 +229,14 @@ def mat2nc_ins_2D(path_run,path_sc=""):
 
 def mat2nc_ins_3D(path_run,path_sc=""):
     """
-    Save nc with dataset instantaneous 3D
+    Save nc with dataset instantaneous 3D (one nc for each variable)
     Iterates over output requests and create one nc file for every variable.
-    path_sc is the path of SWASH source code if using developer mode.    
+    path_sc is the path of SWASH source code if using developer mode.
     """
     import scipy.io as sio
     import xarray as xr
     import numpy as np
-    frame,reqn,nV=load_req(path_run)
+    frame,reqn,nV,_,_=load_req(path_run)
     swash_dict,attr_dict=swashdict(path_sc=path_sc)
     ivl=[] # var, y,  x and output (.mat)
     [[ivl.append([j,frame.get(i[0],['']*2)[0],frame.get(i[0],['']*2)[1],i[2]]) for j in  i[3] if j not in ['XP','YP','BOTL'] ] for i in reqn if i[1]=="inst"] # instantaneous variables list
@@ -274,6 +286,75 @@ def mat2nc_ins_3D(path_run,path_sc=""):
         ds.t.attrs = {"standard_name": swash_dict['Tsec'][0],"long_name": swash_dict['Tsec'][1], "units": swash_dict['Tsec'][2],"axis":"t"}        
         print(f"Saving ins_3D_{i[0]}_{i[3].split('.')[0].split('/')[-1]}.nc")
         ds.to_netcdf(path_run+f"ins_3D_{i[0]}_{i[3].split('.')[0].split('/')[-1]}.nc")
+
+def mat2nc_ins_table(path_run):
+    """
+    Save nc with dataset instantaneous table (does not work for vector output)
+    It saves one netcdf file per line request.
+    """
+    import scipy.io as sio
+    import xarray as xr
+    import numpy as np
+    _,_,_,reqtable,point=load_req(path_run)
+    swash_dict,attr_dict=swashdict()
+    swash_dict_rev={j[0]:[i]+j[1:] for i,j in swash_dict.items()}
+    for tab in reqtable: #name, of grid type (mean or inst), name of tbl, list of requested variables
+        out={}
+        if  open(path_run+tab[2]).readlines()[0][:5]!="SWASH": # no header 
+            for idx,var in enumerate(tab[3]):
+                out[var]= np.transpose(np.reshape(np.array([float(j[idx]) for j in [i.strip().split()
+                    for i in open(path_run+tab[2]).readlines()]]),
+                    (len(open(path_run+tab[2]).readlines())//
+                    len(point[tab[0]][0]),len(point[tab[0]][0])))) # station,time
+        else: # with header --> reads number of dimensions in vertical for each variable
+            ndim=[int(i) for i in [i.split()[0] for i in open(path_run+tab[2]).readlines()][8:8+4*(len(tab[-1])-1)+1][::4]]
+            outarray=np.concatenate([[float(k) for k in j.strip().split()] for j in open(path_run+tab[2]).readlines()[8+4*(len(tab[-1])-1)+2:]]) # one line
+            outlist=[]
+            for idx,var in enumerate(tab[3]): # parse into text file and append list for each variable
+                outlist.append([])
+                i=0
+                for j in range(idx): i+=ndim[j]
+                while i<len(outarray):
+                    for j in range(ndim[idx]):
+                        outlist[idx].append(outarray[i])
+                        i+=1
+                    i+=int(np.dot(ndim,np.ones(len(ndim)))-ndim[idx])
+                if ndim[idx]==1:
+                    out[var]=np.transpose(np.reshape(np.array(outlist[idx]),(len(outlist[idx])//len(point[tab[0]][0]),len(point[tab[0]][0]))))
+                else:
+                    out[var]=np.transpose(np.reshape(np.array(outlist[idx]),(len(outlist[idx])//len(point[tab[0]][0])//ndim[idx],len(point[tab[0]][0]),ndim[idx])),(1,0,2))
+        
+        var_2D = [i for i,j in zip(tab[-1],ndim) if j==1]
+        ds_2D=xr.Dataset(
+                    data_vars={swash_dict_rev.get(key,[''])[0]: (("stations", "time"), value,{"standard_name": swash_dict_rev.get(key,[''])[0],
+                                                                "long_name": swash_dict_rev.get(key,['']*2)[1],
+                                                                "units": swash_dict_rev.get(key,['']*3)[2]})
+                            for key,value in out.items() if key in var_2D},
+                            attrs=attr_dict
+                    )
+        ds_3D=xr.Dataset(
+                    data_vars={swash_dict_rev.get(key,[''])[0]: (("stations","time","k"), value,{"standard_name": swash_dict_rev.get(key,[''])[0],
+                                                                "long_name": swash_dict_rev.get(key,['']*2)[1],
+                                                                "units": swash_dict_rev.get(key,['']*3)[2]})
+                            for key,value in out.items() if key not in ('XP','YP','TIME','TSEC')
+                                                            and key not in var_2D},
+                            attrs=attr_dict
+                    )
+        ds=xr.merge([ds_2D,ds_3D])
+        if 'TIME' in out.keys():
+            t="Time"
+            ds=ds.assign_coords(time=out["TIME"][0,:])
+            ds.time.attrs = {"standard_name": swash_dict[t][0],"long_name": swash_dict[t][1], "units": swash_dict[t][2],"axis":"time"}
+        elif 'TSEC' in out:
+            t="Tsec"
+            ds=ds.assign_coords(time=out["TSEC"][0,:])
+            ds.time.attrs = {"standard_name": swash_dict[t][0],"long_name": swash_dict[t][1], "units": swash_dict[t][2],"axis":"time"}
+        ds['station_y']=(("stations"),point[tab[0]][0])
+        ds['station_x']=(("stations"),point[tab[0]][1])
+        ds.station_y.attrs = {"standard_name": swash_dict['Yp'][0],"long_name": swash_dict['Yp'][1], "units": swash_dict['Yp'][2],"axis":"Y"}
+        ds.station_x.attrs = {"standard_name": swash_dict['Xp'][0],"long_name": swash_dict['Xp'][1], "units": swash_dict['Xp'][2],"axis":"X"}    
+        print(f"Saving ins 2D table {tab[2].split('.')[0]}.nc")
+        ds.to_netcdf(path_run+f"{tab[2].split('.')[0]}.nc")
 
 if __name__ == '__main__':
     pass
