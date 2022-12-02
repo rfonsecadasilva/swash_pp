5bce0522b9f97240e5e91d400bb6e3ce5984cd5b
Renan Silva <00099335@uwa.edu.au>
2022-12-02 10:53:15 +0800

    Update swash_wl_gif.py
    add gif_path argument

--- src/swash_pp/swash_wl_gif.py
+++ src/swash_pp/swash_wl_gif.py
@@ -2,7 +2,7 @@ import warnings, math, os
 import matplotlib.pyplot as plt
 import imageio
 
-def create_gif(gif_name,ds,xmin,xmax,tmin,tmax,dt,zmin,zmax):
+def create_gif(gif_name,ds,xmin,xmax,tmin,tmax,dt,zmin,zmax,gif_path=""):
     """
     Create gif with water level animation
     Args:
@@ -15,10 +15,11 @@ def create_gif(gif_name,ds,xmin,xmax,tmin,tmax,dt,zmin,zmax):
         dt (float): time step (s)
         zmin (float): minimum z-position (m)
         zmax (float): maximum z-position (m)
+        gif_path (str): path where to save gif. Default to "" (local path).
     """
     
     warnings.filterwarnings('ignore')
-    if not os.path.exists(gif_name): os.mkdir(gif_name)
+    if not os.path.exists(gif_name): os.mkdir(f'{gif_path}{gif_name}')
     temp=temp=ds.sel(x=slice(xmin,xmax),t=slice(tmin,tmax,math.ceil(dt/((ds.t.isel(t=1)-ds.t.isel(t=0)).values.item()))))
     d=temp.Botlev # bottom level
     wl=temp.Watlev # water level
@@ -40,7 +41,7 @@ def create_gif(gif_name,ds,xmin,xmax,tmin,tmax,dt,zmin,zmax):
             [(i.set_xticklabels(""),i.set_yticklabels(""),i.axis([ibp.t.isel(t=0),ibp.t.isel(t=-1),ibp.min().item(),ibp.max().item()]),i.set_xlabel("t"),i.set_ylabel("z (SWL)"),i.grid("on"),i.axhline(y=ibp.mean().item(),c="k",ls="--",lw=2)) for i in [aax]]
             aax.plot([ibp.isel(t=t).t.item()],[ibp.isel(t=t).item()],ls="",marker="o",c="r")
             aax.plot(ibp.isel(t=slice(0,t+1)).t.values,ibp.isel(t=slice(0,t+1)).values,ls="-",marker="",c="r")
-        plt.savefig(f'{gif_name}/{gif_name}_Fig_{t:04d}.png',dpi=100)
+        plt.savefig(f'{gif_path}{gif_name}/{gif_name}_Fig_{t:04d}.png',dpi=100)
         plt.close() 
 
     def frames_gif(gf,dt):
@@ -49,17 +50,17 @@ def create_gif(gif_name,ds,xmin,xmax,tmin,tmax,dt,zmin,zmax):
         """
         
         frames=[]
-        for filename in [f'{gif_name}/{gif_name}_Fig_{t:04d}.png'  for t in range(gf)]:
+        for filename in [f'{gif_path}{gif_name}/{gif_name}_Fig_{t:04d}.png'  for t in range(gf)]:
             frames.append(imageio.imread(filename))
-        imageio.mimsave(f'{gif_name}.gif', frames, 'GIF', duration=dt)          
+        imageio.mimsave(f'{gif_path}{gif_name}.gif', frames, 'GIF', duration=dt)          
 
     def delete_fig(gf):
         """
         Delete original figures
         """
-        for filename in [f'{gif_name}/{gif_name}_Fig_{t:04d}.png'  for t in range(gf)]:
+        for filename in [f'{gif_path}{gif_name}/{gif_name}_Fig_{t:04d}.png'  for t in range(gf)]:
             os.remove(filename)
-        os.rmdir(gif_name)
+        os.rmdir(f'{gif_path}{gif_name}')
         
     print(f"Creating figures - total of {len(temp.t)} time steps")
     for t in range(len(temp.t)):
