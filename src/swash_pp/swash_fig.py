878e910504c298f045634d96afa1cc62b27d7515
Renan Silva <00099335@uwa.edu.au>
2023-03-07 21:38:36 +0800

    Update swash_fig.py
    - Mfv_fig

--- src/swash_pp/swash_fig.py
+++ src/swash_pp/swash_fig.py
@@ -44,12 +44,81 @@ def Hs_Mfv_fig(ds,xmin=None,xmax=None,dx=None,ymin=None,ymax=None,dy=None,scale=
     if vel_clip_min:
         vel_clip_min=xr.apply_ufunc(np.abs,temp["Mfvx"]).quantile(vel_clip_min).item()
         temp=temp.where(lambda x:(x["Mfvx"]**2+x["Mfvy"]**2)**0.5>=vel_clip_min,drop=True)
+    else: vel_clip_max=xr.apply_ufunc(np.abs,temp["Mfvx"]).quantile(0.9).item()
     fig,ax=plt.subplots(figsize=(9.2,5.2))
     ax=[ax]
     ax[0].axis('equal')
     # plot hs
     hs=ds["Hsig"].plot(ax=ax[0],cmap=cmap,levels=hs_levels,add_colorbar=False,clip_on=True)
     fig.colorbar(hs,ticks=hs_ticks,label=[r'$\mathrm{ H_S}$ [m]',r'$\mathrm{ H_S\,/\,H_{S,0}}$'][Hs0 is not None],orientation='horizontal',cax=ax[0].inset_axes([0.05,1.05,0.85,0.05],transform=ax[0].transAxes),ticklocation='top')
+    # plot land surface
+    ax[0].fill_betweenx(ds.y,ds.isel(y=0).where(lambda x:x.Botlev<=0,drop=True).isel(x=0).x.item(),xmax,color="peachpuff",clip_on=True)
+    # plot reef contour
+    if plot_dep_dev:
+        (-(ds.where(ds.Botlev-ds.isel(y=0).Botlev!=0,drop=True).Botlev)).plot.contourf(ax=ax[0],colors="k",add_colorbar=False)
+    # plot quiver with Mfv
+    quiv=temp.plot.quiver(ax=ax[0],x="x",y="y",u="Mfvx",v="Mfvy",scale=scale,add_guide=False)
+    ax[0].quiverkey(quiv,0.95,1.02,vel_clip_max*scale,f"{vel_clip_max:.2f} m/s")
+    # plot depth contour
+    if dep_levels is not None:
+        x_dep_levels=[ds.isel(y=-1).where(lambda x:x["Botlev"]<=i,drop=True).isel(x=0).x.item() for i in dep_levels]
+        depcont=(ds['Botlev']).plot.contour(levels=dep_levels,colors='grey',linewidth=3,linestyles="-",ax=ax[0])
+        ax[0].clabel(depcont,fmt='-%.2f m',manual=[(i,ymin+(ymax-ymin)*0.9) for i in x_dep_levels],fontsize=14)
+    # set axis properties
+    ax[0].set_title("")
+    [(i.set_xlabel('X [m]'),i.set_ylabel('Y [m]')) for i in ax]
+    [(i.set_xlim([xmin,xmax]),i.set_ylim([ymin,ymax])) for i in ax]
+    plt.close()
+    return fig
+
+def Mfv_fig(ds,xmin=None,xmax=None,dx=None,ymin=None,ymax=None,dy=None,scale=None,vel_clip_max=None,vel_clip_min=None,dpi=100,plot_dep_dev=False,dep_levels=None,mfvy_levels=None,mfvy_ticks=None,cmap="RdBu_r"):
+    """
+    Create 2D fig with mass flux velocities and y-component as colours.
+    Args:
+        ds (xr data structure): Single data structure with 'x', 'Botlev', 'Mfvx' and Mfvy'.
+        xmin (float, optional): minimum x-position (m). If None, ds.x.min().
+        xmax (float, optional): maximum x-position (m). If None, ds.x.max().
+        ymin (float, optional): minimum y-position (m). If None, ds.y.min().
+        ymax (float, optional): maximum y-position (m). If None, ds.y.max().
+        scale (float, optional): minimum threshold depth for runup calculation (m). Default to 1.
+        vel_clip_max (float, optional): maximum x- and y-velocity clip (in % of x quantile). Default to None (i.e., no clipping).
+        vel_clip_min (float, optional): minimum absolute velocity (in % of x quantile) to be plotted (otherwise nan). Default to None
+        dpi (int, optional): Image dpi. Default to 100.
+        plot_dep_dev (bool), optional: Condition plotting deviations from depth at the first cross-shore section. Default to False.        
+        dep_levels(np array, optional): array with depth contour levels (in m) to be plotted. If None, no depth contours.
+        mfvy_levels(np array, optional): array with significant wave height contour levels (in m) to be plotted.
+        mfvy_ticks(np array, optional): array with significant wave height contour levels ticks (in m) to be plotted.
+        cmap (str): matplotlib colour map. Default to "RdBu_r".
+    """
+    warnings.filterwarnings('ignore')
+    # remove Mfvy=0 (as this is a paralelisation issue)
+    #ds=ds.where(lambda x:x["Mfvy"]!=0,drop=True)    
+    # Assign xmin, xmax, dx, ymin, ymax, dy, tmin, tmax, and dt if not defined
+    xmin = xmin or ds.x.min().item()
+    xmax = xmax or ds.x.max().item()
+    dx = dx or (ds.x.isel(x=1)-ds.x.isel(x=0)).item()
+    ymin = ymin or ds.y.min().item()
+    ymax = ymax or ds.y.max().item()    
+    dy = dy or (ds.y.isel(y=1)-ds.y.isel(y=0)).item()    
+    temp=ds.sel(x=slice(xmin,xmax,math.ceil(dx/((ds.x.isel(x=1)-ds.x.isel(x=0)).values.item()))),
+                y=slice(ymin,ymax,math.ceil(dy/((ds.y.isel(y=1)-ds.y.isel(y=0)).values.item()))))
+    mfvy_max=xr.apply_ufunc(np.abs,temp["Mfvy"]).max().item()
+    mfvy_levels = mfvy_levels or np.arange(-mfvy_max,mfvy_max+mfvy_max/50,mfvy_max/50)
+    mfvy_ticks = mfvy_ticks or np.around(np.arange(-mfvy_max,mfvy_max+mfvy_max/3,mfvy_max/3),decimals=2)
+    if vel_clip_max:
+            vel_clip_max=xr.apply_ufunc(np.abs,temp["Mfvx"]).quantile(vel_clip_max).item()
+            temp["Mfvx"]=temp["Mfvx"].clip(min=-vel_clip_max,max=vel_clip_max)
+            temp["Mfvy"]=temp["Mfvy"].clip(min=-vel_clip_max,max=vel_clip_max)
+    else: vel_clip_max=xr.apply_ufunc(np.abs,temp["Mfvx"]).quantile(0.9).item()
+    if vel_clip_min:
+        vel_clip_min=xr.apply_ufunc(np.abs,temp["Mfvx"]).quantile(vel_clip_min).item()
+        temp=temp.where(lambda x:(x["Mfvx"]**2+x["Mfvy"]**2)**0.5>=vel_clip_min,drop=True)
+    fig,ax=plt.subplots(figsize=(9.2,5.2))
+    ax=[ax]
+    ax[0].axis('equal')
+    # plot hs
+    mfvy=ds["Mfvx"].plot(ax=ax[0],cmap=cmap,levels=mfvy_levels,add_colorbar=False,clip_on=True)
+    fig.colorbar(mfvy,ticks=mfvy_ticks,label=r'$\mathrm{ V [m \cdot s^{-1}]}$ ',orientation='horizontal',cax=ax[0].inset_axes([0.05,1.05,0.85,0.05],transform=ax[0].transAxes),ticklocation='top')
     # plot land surface
     ax[0].fill_betweenx(ds.y,ds.isel(y=0).where(lambda x:x.Botlev<=0,drop=True).isel(x=0).x.item(),xmax,color="peachpuff",clip_on=True)
     # plot reef contour
