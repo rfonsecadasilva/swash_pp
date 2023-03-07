75e17935ad0076de3a11617f712c567647daf52a
Renan Silva <00099335@uwa.edu.au>
2023-03-07 11:42:15 +0800

    Update swash_wl_gif.py
    3D_gif changes
    - add yc (coordinate position where to plot cross-shore view)
    - change seabed contour
    - remove xmax plane
    - add return_fig as argument
    
    create_mamfv_gif

--- src/swash_pp/swash_wl_gif.py
+++ src/swash_pp/swash_wl_gif.py
@@ -90,8 +90,8 @@ def create_2D_gif(gif_name,ds,xmin=None,xmax=None,tmin=None,tmax=None,dt=None,zm
 
 def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,tmax=None,dt=None,zmin=None,zmax=None,
                   aspect_ratio=None,
-                  vmin=None,vmax=None,sc_wlv=1,elev=50,elev_light=1,azim=-135,azim_light=-155,depmin=None,
-                  acc_factor=1,gif_path="",axis_off=False,dpi=100,plot_dep_dev=False,combine_only=False,tpar=-1):
+                  vmin=None,vmax=None,sc_wlv=1,elev=50,elev_light=50,azim=-135,azim_light=-155,depmin=None,
+                  acc_factor=1,gif_path="",axis_off=False,dpi=100,plot_dep_dev=False,yc=0,combine_only=False,tpar=-1,return_fig=False):
     """
     Create 3D gif with water level animation
     Args:
@@ -120,8 +120,10 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
         axis_off (bool, optional): boolean for plotting axis. Default to False (i.e., axis is plotted).
         dpi (int): Image dpi. Default to 100.
         plot_dep_dev (bool): Condition plotting deviations from depth at the first cross-shore section. Default to False.
+        yc (float): Y-coordinate of position where to plot cross-shore view and respective lines on 3D plot (in m). Default to 0.
         combine_only (bool): Condition for not generating frames but only combining them. Default to False.
         tpar (int): If different than -1, only generate frame for index tpar (for parallel computation). Default to -1.
+        return_fig(bool): If True, return fig for index tpar. Default to False.
     
         For parallel computing, the argument tpar can be used across a parallelized time loop. For example:
     '''
@@ -170,17 +172,17 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
     zorder=[30,40,5,60]
     def wlv_1d(ax,t=0): # upper 2D plot
         # plot land and water level surfaces
-        ax.fill_between(temp.x,zmin+0*(-temp.Botlev.isel(y=len(temp.y)//2)).values.squeeze(),(-temp.Botlev.isel(y=len(temp.y)//2)).values.squeeze(),color="peachpuff") # contour beach
-        ax.fill_between(temp.x,(-temp.Botlev.isel(y=len(temp.y)//2)).values.squeeze(),temp.Watlev.isel(y=len(temp.y)//2,t=t).values.squeeze(),color="skyblue",alpha=0.5)        
-        # plot depth line and scatter at centre y
-        ax.plot(np.concatenate(([xmin],temp.x.values)),np.concatenate(([temp.isel(x=0,y=len(temp.y)//2,t=t).Watlev.item()*sc_wlv],-temp.isel(y=len(temp.y)//2).Botlev.values)),'k--')
-        xr.plot.scatter(temp.isel(y=len(temp.y)//2,t=t,x=0),ax=ax,x="x",y="Watlev",c="k",s=200)
+        ax.fill_between(temp.x,zmin+0*(-temp.Botlev.sel(y=yc,method="nearest")).values.squeeze(),(-temp.Botlev.sel(y=yc,method="nearest")).values.squeeze(),color="peachpuff") # contour beach
+        ax.fill_between(temp.x,(-temp.Botlev.sel(y=yc,method="nearest")).values.squeeze(),temp.Watlev.sel(y=yc,method="nearest").isel(t=t).values.squeeze(),color="skyblue",alpha=0.5)        
+        # plot depth line and scatter at yc
+        ax.plot(np.concatenate(([xmin],temp.x.values)),np.concatenate(([temp.sel(y=yc,method="nearest").isel(x=0,t=t).Watlev.item()*sc_wlv],-temp.sel(y=yc,method="nearest").Botlev.values)),'k--')
+        ax.scatter(temp.sel(y=yc,method="nearest").isel(t=t,x=0).x,temp.sel(y=yc,method="nearest").isel(t=t,x=0).Watlev,c="k",s=200)
         if depmin: # runup line and scatter
-            ax.plot([xrunup.isel(y=len(temp.y)//2,t=t).item()]*2,[zmin,temp.Ibp.isel(y=len(temp.y)//2,t=t).item()],c='r',ls='--')
-            ax.scatter(xrunup.isel(y=len(temp.y)//2,t=t),temp.isel(y=len(temp.y)//2,t=t).Ibp,c="r",s=200)
+            ax.plot([xrunup.sel(y=yc,method="nearest").isel(t=t).item()]*2,[zmin,temp.Ibp.sel(y=yc,method="nearest").isel(t=t).item()],c='r',ls='--')
+            ax.scatter(xrunup.sel(y=yc,method="nearest").isel(t=t),temp.sel(y=yc,method="nearest").isel(t=t).Ibp,c="r",s=200)
             aax=ax.inset_axes(
                 [0.7,0.2,0.2,0.2], transform=ax.transAxes)
-            ibp=temp.Ibp.isel(y=len(temp.y)//2)
+            ibp=temp.Ibp.sel(y=yc,method="nearest")
             if axis_off:
                 [(i.set_xticklabels(""),i.set_yticklabels(""),i.axis([ibp.t.isel(t=0),ibp.t.isel(t=-1),ibpmin,ibpmax]),i.set_xlabel("t"),i.set_ylabel("z (SWL)"),i.grid("on"),i.axhline(y=0,c="k",ls="--",lw=2)) for i in [aax]]
             else:
@@ -199,14 +201,14 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
     def data_gen(ax,t=0): # lower 3D plot
         # create seabed colormap
         cmapb = 'YlOrBr_r'
-        plt.cm.register_cmap(name='seabed2',cmap=colors.LinearSegmentedColormap.from_list('seabed',
+        plt.cm.register_cmap(name='seabed',cmap=colors.LinearSegmentedColormap.from_list('seabed',
                         [(0,plt.cm.get_cmap(cmapb)(int(0.65*255))),
-                        (1,plt.cm.get_cmap(cmapb)(int(.80*255)))]
+                        (1,plt.cm.get_cmap(cmapb)(int(0.8*255)))]
                         ))
         # plot seabed
-        (-temp.Botlev).plot.surface(ax=ax,rstride=1,cstride=1,cmap=plt.cm.get_cmap('seabed2',(zmax-zmin)*4),vmax=zmax,vmin=zmin,alpha=0.9,zorder=zorder[0],add_colorbar=False) #seabed with increased arrays (to make it look nicer)        
+        (-temp.Botlev).plot.surface(ax=ax,rstride=1,cstride=1,cmap=plt.cm.get_cmap('seabed'),vmax=zmax,vmin=zmin,alpha=0.9,zorder=zorder[0],add_colorbar=False) #seabed with increased arrays (to make it look nicer)
         if plot_dep_dev: # plot deviation from first line of seabed
-            (-(ds.where(ds.Botlev-ds.isel(y=0).Botlev!=0,drop=True).Botlev)).plot.surface(ax=ax,rstride=1,cstride=1,color='orangered',zorder=500,add_colorbar=False)      
+            (-(temp.where(temp.Botlev-temp.isel(y=-1).Botlev!=0,drop=True).Botlev)).plot.surface(ax=ax,add_colorbar=False,zorder=500,color="orangered")      
         # select water level colormap
         colmap=plt.cm.get_cmap('Blues',256)
         colmap.set_bad(color='w',alpha=0) #set color for nan
@@ -216,9 +218,9 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
         (sc_wlv*temp.Watlev.isel(t=t)).plot.surface(ax=ax,rstride=1, cstride=1, linewidth=0,
                         antialiased=False, facecolors=light.shade(np.ma.masked_array(temp.Watlev.isel(t=t),mask=M),cmap=colmap,vmin=vmin*sc_wlv,vmax=vmax*sc_wlv),
                         zorder=zorder[1],alpha=.8,add_colorbar=False)
-        # plot water level line and scatter at centre y and start of x, and profile of depth at centre y
-        ax.plot(np.concatenate(([xmin],temp.x.values)),[temp.isel(y=len(temp.y)//2).y.values]*(len(temp.x)+1),np.concatenate(([temp.isel(x=0,y=len(temp.y)//2,t=t).Watlev.item()*sc_wlv],-temp.isel(y=len(temp.y)//2).Botlev.values)),'k--',zorder=zorder[2],alpha=1)
-        ax.scatter(temp.isel(x=0).x,temp.isel(y=len(temp.y)//2).y,temp.isel(y=len(temp.y)//2,t=t,x=0).Watlev*sc_wlv,s=200,c='k')
+        # plot water level line and scatter at yc and start of x, and profile of depth at yc
+        ax.plot(np.concatenate(([xmin],temp.x.values)),[temp.sel(y=yc,method="nearest").y.values]*(len(temp.x)+1),np.concatenate(([temp.sel(y=yc,method="nearest").isel(x=0,t=t).Watlev.item()*sc_wlv],-temp.sel(y=yc,method="nearest").Botlev.values)),'k--',zorder=zorder[2],alpha=1)
+        ax.scatter(temp.isel(x=0).x,temp.sel(y=yc,method="nearest").y,temp.sel(y=yc,method="nearest").isel(t=t,x=0).Watlev*sc_wlv,s=200,c='k')
         if depmin: # plot runup line (scatter not possible to see)
             ax.plot(xrunup.isel(t=t).values,xrunup.isel(t=t).y.values,temp.Ibp.isel(t=t).values*sc_wlv,'r-',zorder=zorder[3])
         # plot ymin and ymax planes
@@ -228,6 +230,7 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
                                 facecolor='burlywood'))
         # plot xmax plane
         for x in [-1]:
+            break
             ax.add_collection3d(Poly3DCollection([[(i.x.item(),i.y.item(),zmin) for i in temp.isel(x=x,y=slice(None,None,len(temp.y)-1),t=0).Watlev]+\
                      [(i.x.item(),i.y.item(),zmax) for i in temp.isel(x=x,y=slice(None,None,-len(temp.y)+1),t=0).Watlev]],
                                 facecolor='burlywood'))            
@@ -249,6 +252,9 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
         specc = fig.add_gridspec(ncols=1,nrows=2,height_ratios=[1.5,5])
         wlv_1d(fig.add_subplot(specc[0]),t=t)
         data_gen(fig.add_subplot(specc[1],projection='3d'),t=t)
+        if return_fig:
+            #plt.close(fig)
+            return fig
         plt.savefig(f'{gif_path}{gif_name}/{gif_name}_Fig_{t:04d}.png',dpi=dpi,bbox_inches='tight')
         plt.close()
 
@@ -264,8 +270,9 @@ def create_3D_gif(gif_name,ds,xmin=None,xmax=None,ymin=None,ymax=None,tmin=None,
             delete_fig(gf=len(temp.t),gif_name=gif_name,gif_path=gif_path)
         else:
             #print(f"{tpar+1}/{len(temp.t)}") # for debugging
-            number_pngs=len([i for i in os.listdir(f'{gif_path}{gif_name}/') if i.split('.')[-1]=='png'])
-            if number_pngs%10==0: print(f"{number_pngs/len(temp.t)*100:.1f} %")
+            if not return_fig:
+                number_pngs=len([i for i in os.listdir(f'{gif_path}{gif_name}/') if i.split('.')[-1]=='png'])
+                if number_pngs%10==0: print(f"{number_pngs/len(temp.t)*100:.1f} %")
             fig_rp(t=tpar)       
     else:
             print(f"Creating gif")
@@ -365,6 +372,81 @@ def create_2D_comp_gif(gif_name,ds,label,xmin=None,xmax=None,tmin=None,tmax=None
     frames_gif(gf=len(temp[0].t),dt=dt,gif_name=gif_name,gif_path=gif_path)
     print(f"Deleting figures")
     delete_fig(gf=len(temp[0].t),gif_name=gif_name,gif_path=gif_path)
+
+def create_mamfv_gif(gif_name,ds,xmin=None,xmax=None,dx=None,ymin=None,ymax=None,dy=None,tmin=None,tmax=None,dt=None,scale=None,vel_clip_max=None,vel_clip_min=None,acc_factor=1,gif_path="",axis_off=False,dpi=100,plot_dep_dev=False):
+    """
+    Create 2D gif with moving average of mass flux velocities animation
+    Args:
+        gif_name (str): Gif name
+        ds (xr data structure): Single data structure with 'x', 'Botlev', 'Mamfvx' and Mamfvx'.
+        xmin (float): minimum x-position (m). If None, ds.x.min().
+        xmax (float): maximum x-position (m). If None, ds.x.max().
+        ymin (float, optional): minimum y-position (m). If None, ds.y.min().
+        ymax (float, optional): maximum y-position (m). If None, ds.y.max().
+        tmin (float): minimum time (s). If None, ds.t.min().
+        tmax (float): maximum time (s). If None, ds.t.max().
+        dt (float): time step (s). If None, (ds.t.isel(t=1)-ds.t.isel(t=0)).
+        scale (float, optional): minimum threshold depth for runup calculation (m). Default to 1.
+        vel_clip_max (float, optional): maximum x- and y-velocity clip (in m/s). Default to None (i.e., no clipping).
+        vel_clip_min (float, optional): minimum absolute velocity (in m/s) to be plotted (otherwise nan). Default to None
+        acc_factor (float, optional): Ratio of frame duration by dt. Default to 1.
+        gif_path (str, optional): path where to save gif. Default to "" (local path).
+        axis_off (bool, optional): boolean for plotting axis. Default to False (i.e., axis is plotted).
+        dpi (int): Image dpi. Default to 100.
+        plot_dep_dev (bool): Condition plotting deviations from depth at the first cross-shore section. Default to False.        
+    """
+    warnings.filterwarnings('ignore')
+    # Assign xmin, xmax, dx, ymin, ymax, dy, tmin, tmax, and dt if not defined
+    xmin = xmin or ds.x.min().item()
+    xmax = xmax or ds.x.max().item()
+    dx = dx or (ds.x.isel(x=1)-ds.x.isel(x=0)).item()
+    ymin = ymin or ds.y.min().item()
+    ymax = ymax or ds.y.max().item()    
+    dy = dy or (ds.y.isel(y=1)-ds.y.isel(y=0)).item()    
+    tmin = tmin or ds.t.min().item()
+    tmax = tmax or ds.t.max().item()
+    dt = dt or (ds.t.isel(t=1)-ds.t.isel(t=0)).item()
+    temp=ds.sel(x=slice(xmin,xmax,math.ceil(dx/((ds.x.isel(x=1)-ds.x.isel(x=0)).values.item()))),
+                y=slice(ymin,ymax,math.ceil(dy/((ds.y.isel(y=1)-ds.y.isel(y=0)).values.item()))),
+                t=slice(tmin,tmax,math.ceil(dt/((ds.t.isel(t=1)-ds.t.isel(t=0)).values.item()))))
+    if vel_clip_max:
+        temp["Mamfvx"]=temp["Mamfvx"].clip(min=-vel_clip_max,max=vel_clip_max)
+        temp["Mamfvy"]=temp["Mamfvy"].clip(min=-vel_clip_max,max=vel_clip_max)
+    if vel_clip_min:
+        temp=temp.where(lambda x:(x["Mamfvx"]**2+x["Mamfvy"]**2)**0.5>=vel_clip_min,drop=True)
+    if not os.path.exists(gif_name): os.mkdir(f'{gif_path}{gif_name}')
+    def fig_rp(t=0):
+        _,ax=plt.subplots(constrained_layout=True,figsize=(10,6))
+        ax=[ax]
+        ax[0].axis('equal')
+        # plot land surface
+        ax[0].fill_betweenx(temp.y,temp.isel(y=0).where(lambda x:x.Botlev<=0,drop=True).isel(x=0).x.item(),xmax,color="peachpuff")
+        # plot reef contour
+        if plot_dep_dev:
+            (-(ds.where(ds.Botlev-ds.isel(y=0).Botlev!=0,drop=True).Botlev)).plot.contourf(ax=ax[0],colors="k",add_colorbar=False,alpha=0.7)
+        # plot quiver with mamfv
+        temp.isel(t=t).plot.quiver(ax=ax[0],x="x",y="y",u="Mamfvx",v="Mamfvy",scale=scale,add_guide=False)
+        # set axis properties
+        ax[0].set_title("")
+        if axis_off:
+            ax[0].axis('off')
+        else:
+            [(i.set_xlabel('X [m]'),i.set_ylabel('Y [m]')) for i in ax]
+            ax[0].text(0.5,1.1,'t = {:.1f} s'.format(temp.isel(t=t).t.item()),transform=ax[0].transAxes,ha='center',va='bottom') # print time
+        [(i.set_xlim([xmin,xmax]),i.set_ylim([ymin,ymax])) for i in ax]
+        plt.savefig(f'{gif_path}{gif_name}/{gif_name}_Fig_{t:04d}.png',dpi=dpi)
+        plt.close() 
+
+    print(f"Creating figures - total of {len(temp.t)} time steps")
+    for t in range(len(temp.t)):
+        if t%50==0: print(f"{t+1}/{len(temp.t)}")
+        fig_rp(t=t)
+        
+    print(f"Creating gif")
+    frames_gif(gf=len(temp.t),dt=1,gif_name=gif_name,gif_path=gif_path,acc_factor=acc_factor)
+    print(f"Deleting figures")
+    delete_fig(gf=len(temp.t),gif_name=gif_name,gif_path=gif_path)
+
 
 if __name__ == '__main__':
     pass
