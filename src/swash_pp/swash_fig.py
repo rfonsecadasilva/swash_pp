7207a002696a1b84c8f35b05f6a7b42102261798
Renan Silva <00099335@uwa.edu.au>
2023-03-08 16:28:51 +0800

    Update swash_fig.py
    - Hs_wm_fig

--- src/swash_pp/swash_fig.py
+++ src/swash_pp/swash_fig.py
@@ -199,7 +199,7 @@ def Hs_Theta_fig(ds,xmin=None,xmax=None,dx=None,ymin=None,ymax=None,dy=None,scal
     return fig
 
 
-def plot_dt(path_run,print="PRINT",Tp=None):
+def dt_fig(path_run,print="PRINT",Tp=None):
     """Plot time series of dt based on PRINT file
 
     Args:
@@ -263,6 +263,30 @@ def ener_enst_fig(path_run,ener_enstro="ener_enstro",Tp=None):
     plt.close()
     return fig
 
+def Hs_wm_fig(da,dt=0.1,burst=30,Tp=2.83,Hs_wm=0.118):
+    """Plot time series of modelled (total) Hs with prescribed time window.
+    Note: later separate incoming and outgoing components.
+
+    Args:
+        da (xr DataArray): DataArray with 'Watlev' (water levels at wave maker) (in m)
+        dt (float, optional): time step (in s). Defaults to 0.1.
+        burst (int, optional): burst length (in Tp). Defaults to 30.
+        Tp (float, optional): peak wave period (in s). Defaults to 2.83.
+        Hs_wm (float, optional): target significant wave height at the wave maker (in m). Defaults to 0.118.
+    """
+    da=da.assign_coords(time=da.time*dt) # assign time axis
+    da.time.attrs = {"standard_name": 'Tsec',"long_name":"Time in seconds from reference time","units":"s","axis":"time"}    
+    xmin,xmax,dx=da.time.min().item(),np.around(da.time.max().item()),200 #time axis properties
+    # figure
+    fig,ax=plt.subplots(figsize=(10,7))
+    (4*da.Watlev.rolling(time=int(Tp*burst/dt),center=True).std()).plot(ax=ax,c="k",label=f"Modelled (window of {burst:.0f} Tps)")
+    ax.axhline(Hs_wm,c="k",ls="--",label="Target") #prescribed Hs
+    [(i.set_xticks(np.arange(xmin,xmax,dx)),ax.set_ylabel("Hs [m] at wave maker"),i.grid(),i.legend(),i.set_xlim([xmin,xmax])) for i in [ax]]
+    if Tp: # upper x-axis with time in Tp
+        axup=ax.twiny()
+        [(i.set_xticks(np.arange(xmin,xmax,dx)/Tp),i.set_xlim([xmin/Tp,xmax/Tp]),i.set_xlabel("t [Tp]")) for i in [axup]]
+    plt.close()
+    return fig
 if __name__ == '__main__':
     pass
 
